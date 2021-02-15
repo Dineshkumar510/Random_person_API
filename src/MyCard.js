49c85d99bad997a34624433e0308f4fea15e7869
Dineshkumar510 <43378477+Dineshkumar510@users.noreply.github.com>
2021-02-15 22:39:33 +0530

    Add files via upload

---
+++ src/MyCard.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import React from 'react';
+import {Card, CardBody, CardTitle, CardText} from 'reactstrap';
+import {FaEnvelope, FaMapMarkedAlt, FaPhone, FaCalendar} from 'react-icons/fa';
+
+const MyCard = ({details}) => {
+    return (
+        <Card>
+            <CardBody className='text-center'>
+                <img height="150" width = "150" 
+                    className= "rounded-circle img-thumbnail border-danger"
+                    src= {details.picture?.large}/>
+            <CardTitle className="text-primary">
+                <h1>
+                    <span className="pr-2">{details.name?.title}:</span>
+                    <span>{details.name?.first}</span>
+                    <span>{details.name?.last}</span>
+                </h1>
+            </CardTitle>  
+            <CardText>
+                <FaMapMarkedAlt/>
+                   <p>Location: {details.location?.city}</p>
+                <FaPhone/>
+                    <p>Phone: {details.phone}</p>
+                <FaEnvelope/>
+                   <p>email: {details.email}</p>
+                <FaCalendar/>
+                    <p>age: {details.dob?.age}</p>
+            </CardText>  
+            </CardBody>
+        </Card> 
+    );
+};
+ 
+export default MyCard;
